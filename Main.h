6fd536fdef835a45d5e7acd8ddf050c7e9faeb3d
BuildTools <unconfigured@null.spigotmc.org>
2022-02-16 17:01:55 -0600

    Client/server, POSIX messages, structure

---
+++ Main.h
@@ -0,0 +1,41 @@
+// Main header for each source file.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <mqueue.h>
+#include <errno.h>
+
+// Constants
+#define MAX 50
+#define PORT 8000
+#define SA struct sockaddr
+
+// Mqueue globals
+mqd_t mqd;
+struct mq_attr attr; // Only used for buffer size/declaration in mqueue
+char* buffer;
+
+// Player struct
+struct Player
+{
+	//char firstname[50];
+	//char lastname[50];
+	//char country[50];
+	int score;
+	//int num_words;
+	//int num_words_added;
+} Player;
+
+int player();
+void server();
+void client();
+void openMsgQueue();
+void closeMsgQueue();
+void recievePlayerMsg();
---
+++ Player.c
@@ -0,0 +1,12 @@
+// Player struct
+
+#include "Main.h"
+
+int player()
+{
+	int num_players = 3;
+	struct Player* players = malloc(sizeof(Player) * num_players);
+
+	printf("Players made");
+	return 0;
+}
