65bf29563f2a50b26f49a0b1a9ff6089914125d0
therick5 <patricklav@protonmail.com>
2022-02-17 21:13:04 -0600

    Changing things around

--- patricklaverty.c
+++ patricklaverty.c
@@ -1,8 +1,7 @@
 // Author: Patrick Laverty
 // - Server communication
 
-void server()
-{
+void server() {
 	int new_socket;
 	int new_connection;
 	int len;
@@ -11,13 +10,11 @@ void server()
 	struct sockaddr_in cli;
 
 	new_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (new_socket == -1)
-	{
-		printf("Socket creation failed \n");
+	if (new_socket == -1) {
+		printf("Socket creation failed\n");
 	}
-	else
-	{
-		printf("Socket created. \n");
+	else {
+		printf("Socket created.\n");
 	}
 
 	bzero(&server_address, sizeof(server_address));
@@ -26,34 +23,30 @@ void server()
 	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
 	server_address.sin_port = htons(PORT);
 
-	if ((bind(new_socket, (SA*)&server_address, sizeof(server_address))) != 0)
-	{
-		printf("Socket bind failed \n");
+	int binder = bind(new_socket, (SA*)&server_address, sizeof(server_address));
+
+	if binder != 0) {
+		printf("Socket bind failed\n");
 	}
-	else
-	{
-		printf("Socket binded successfully \n");
+	else {
+		printf("Socket binded successfully\n");
 	}
 
-	if ((listen(new_socket, 5)) != 0)
-	{
-		printf("Listen failed \n");
+	if ((listen(new_socket, 5)) != 0) {
+		printf("Listen failed\n");
 	}
-	else
-	{
-		printf("Server listening \n");
+	else {
+		printf("Server listening\n");
 	}
 
 	len = sizeof(cli);
 
 	new_connection = accept(new_socket, (SA*)&cli, &len);
-	if (new_connection < 0)
-	{
-		printf("Server accept failed \n");
+	if (new_connection < 0) {
+		printf("Server accept failed\n");
 	}
-	else
-	{
-		printf("Server accepted the client \n");
+	else {
+		printf("Server accepted the client\n");
 	}
 
 	close(new_socket);
