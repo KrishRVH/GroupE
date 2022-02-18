b9b37beea9e505ea568a11ed7842c6ccbbf721b2
therick5 <patricklav@protonmail.com>
2022-02-17 22:22:22 -0600

    yeehaw

--- patricklaverty.c
+++ patricklaverty.c
@@ -9,45 +9,33 @@ void server() {
 	struct sockaddr_in server_address;
 	struct sockaddr_in cli;
 
+	// Creating Socket
 	new_socket = socket(AF_INET, SOCK_STREAM, 0);
-	if (new_socket == -1) {
-		printf("Socket creation failed\n");
-	}
-	else {
-		printf("Socket created.\n");
-	}
+	if (new_socket == -1) { printf("Socket creation failed\n"); }
+	else { printf("Socket created.\n"); }
 
-	bzero(&server_address, sizeof(server_address));
+	bzero(&server_address, sizeof(server_address)); // Zero out the address space of the struct
 
+
+	// Binding socket to the port
 	server_address.sin_family = AF_INET;
-	server_address.sin_addr.s_addr = htonl(INADDR_ANY);
-	server_address.sin_port = htons(PORT);
+	server_address.sin_addr.s_addr = htonl(INADDR_ANY); // Convert int to ip byte order
+	server_address.sin_port = htons(BINDING_PORT); // Converting ip port to byte order
 
 	int binder = bind(new_socket, (SA*)&server_address, sizeof(server_address));
-	if binder != 0) {
-		printf("Socket bind failed\n");
-	}
-	else {
-		printf("Socket binded successfully\n");
-	}
+	if binder != 0) { printf("Socket bind failed\n"); }
+	else { printf("Socket binded successfully\n"); }
 
+	// Socket listener
 	int listener = listen(new_socket, 5);
-	if (listener != 0) {
-		printf("Listen failed\n");
-	}
-	else {
-		printf("Server listening\n");
-	}
+	if (listener != 0) { printf("Listen failed\n"); }
+	else { printf("Server listening\n"); }
 
+	// Testing connection
 	len = sizeof(cli);
-
 	new_connection = accept(new_socket, (SA*)&cli, &len);
-	if (new_connection < 0) {
-		printf("Server accept failed\n");
-	}
-	else {
-		printf("Server accepted the client\n");
-	}
+	if (new_connection < 0) { printf("Server accept failed\n"); }
+	else { printf("Server accepted the client\n"); }
 
 	close(new_socket);
 }
