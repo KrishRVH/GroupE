6acc86392532ae8ba1c990300dc89f6ce75023ba
adamloec <adamloeckle@gmail.com>
2022-02-23 15:27:32 -0600

    Priority check

--- adamloeckle.c
+++ adamloeckle.c
@@ -61,7 +61,7 @@ void recieveMsg()
 	printf("# Of messages: %ld\n", attr.mq_curmsgs);
 
 	unsigned int priority = 0;
-	if ((mq_receive(mqd, p_buffer, attr.mq_msgsize, &priority)) != -1)
+	while ((mq_receive(mqd, p_buffer, attr.mq_msgsize, &priority)) != -1)
 	{
 		
 		// Multiplayer waiting message, returns 1 if there is a player able to connect, returns 0 if no player or game is going on already
@@ -86,6 +86,7 @@ void recieveMsg()
 		//	struct Player* new_player = (struct Player*)p_buffer;
 		//	printf("Player: %i, Prio: %i\n", new_player[0].score, priority);
 		//}
+		priority = 0;
 	}
 }
 
