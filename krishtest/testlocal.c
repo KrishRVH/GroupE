14a38132e3b920ed1ad2adbdf4eb8080e51dc270
Krish Ravi <vignesh.ravi@okstate.edu>
2022-02-24 14:51:41 -0600

    Implementing dictionary checking, used words functionality for current session, allowed for words longe than 6 letters and incorporated bug fixes

--- krishtest/testlocal.c
+++ krishtest/testlocal.c
@@ -3,16 +3,17 @@
 #include <time.h>
 #include <unistd.h>
 #include <string.h>
+#include <ctype.h>
 void main()
 {
     srand(time(NULL)); 
-    int rng = (rand()%9)+1; //seeding random number from 1 to 10 for first turn
-    int rng2 = 1; //(rand()%9)+1; seeding random number from 1 to 10 for input.txt FOR SOME REASON IT WON'T GENERATE 10
-    char rng2char[2];   
+    int rng = (rand()%10)+1; //seeding random number from 1 to 10 for first turn
+    int rng2 = 1; //(rand()%10)+1; seeding random number from 1 to 10 for input.txt
+    char rng2char[7];   
     sprintf(rng2char, "%d.txt", rng2);
     FILE *fileStream; 
     char letters [6];
-    char fname[12] = "";
+    char fname[14] = "";
     printf("\nrng2 generated was %d",rng2);
     if (rng2==10)
         strcat(fname, "input_");
@@ -26,21 +27,30 @@ void main()
     printf("\nLETTERS ARE: %s",letters);
     int disallowed = 0;
     // CODE TO CHECK VALIDITY OF NEW WORD AGAINST PREVIOUS WORD
-    char prev[6]; //word can never be longer than the 6 available chars
-    char new[6];
+    char prev[100]; //
+    char new[100];
+    char usedWords[100][100];
+    strcpy(usedWords[0],"COOKIE");
+    strcpy(usedWords[1],"HEAVEC");
+    int noUsedWords = 1;
+    for (int i = 0; i<=noUsedWords;i++)
+    {
+        printf("\nUsed word %d of %d is %s",i,noUsedWords,usedWords[i]);
+    }
     printf("\nEnter previous word ");
     gets(prev);
     printf("\nEnter new word ");
     gets(new);
     size_t n = sizeof(prev)/sizeof(char);
     size_t nnew = sizeof(new)/sizeof(char);
+    char *lowernew = malloc(nnew);
     for (int i=0; i<n;i++)
     {
         for (int x = 0; x < nnew && disallowed==0 && new[x]!='\0'; x++)
         {
             for (int y = 0; y < 6 && new[x]!='\0'; y++)
             {
-                printf("\n Iteration %d y iteration %d we're looking at %c in new and %c in letters\n", x, y, new[x], letters[y]);
+                //printf("\n Iteration %d y iteration %d we're looking at %c in new and %c in letters\n", x, y, new[x], letters[y]);
                 if (new[x]!=letters[y])
                 {
                     if (letters[y+1]=='\0')
@@ -61,20 +71,80 @@ void main()
                 int k = 0;
                 while ((j<n) && (new[k]==prev[j]) && !((new[k]=='\0') && (prev[j]=='\0')))
                 {
-                    printf("\n Iteration %d we're looking at %c in new and %c in prev\n", i, prev[j], new[k]);
+                    //printf("\n Iteration %d we're looking at %c in new and %c in prev\n", i, prev[j], new[k]);
                     j++;
                     k++;
-                    printf("The value of j is %d k is %d n is %d", j,k,n);
+                    //printf("The value of j is %d k is %d n is %d", j,k,n);
                 }
                 if ((j==n) || (prev[j]=='\0') || ((new[k]=='\0') && (prev[j]=='\0')))
                 {
                     printf("\n Word is valid!");
-                    //check if word has already been used this game https://www.efaculty.in/c-programs/check-whether-a-given-word-exists-in-a-file-or-not-program-in-c/
+                    //check if word is a dictionary word
+                    printf("\nConverting %s to lower",new);
+                    for(int i = 0; i<nnew; i++)
+                        lowernew[i] = tolower(new[i]);
+                    FILE* filePointerd;
+                    int wordExistd=0;
+                    int bufferLengthd = 255;
+                    char lined[bufferLengthd];
+                    printf("\nChecking if %s is a valid dictionary word",lowernew);
+                    filePointerd = fopen("dictionary.txt", "r");
+                    while(fgets(lined, bufferLengthd, filePointerd))
+                    {
+                        char *ptrd = strstr(lined, lowernew);
+                        if (ptrd != NULL) 
+                        {
+                            wordExistd=1;
+                            break;
+                        }
+                    }
+                    free(lowernew);
+                    fclose(filePointerd);
+                    if (wordExistd==1)
+                    {
+                        printf("\nWord is a valid dictionary word!");
+                    }
+                    else 
+                    {
+                        printf("\nWord is not a valid dictionary word.");
+                        //penalise
+                    }
+                    //check if word has already been used https://stackoverflow.com/questions/63132911/check-if-a-string-is-included-in-an-array-and-append-if-not-c
+                    int dup = 0;      
+                    for (int j = 0; j < 100; j++) 
+                    {
+                        if(strcmp(new, usedWords[j]) == 0) 
+                        {
+                            dup = 1;   // got a duplicate
+                            break;    
+                        }
+                    }
+                    if (dup == 0) {    // not a duplicate: add it to usedWords
+                        strcpy(usedWords[noUsedWords+1], new);
+                        noUsedWords += 1;
+                    }
+                    if(dup) 
+                    {
+                        printf("\nWORD HAS ALREADY BEEN USED THIS GAME.");
+                        //penalise
+                        for (int i = 0; i<=noUsedWords;i++)
+                        {
+                            printf("\nUsed word %d of %d is %s",i,noUsedWords,usedWords[i]);
+                        }
+                    }
+                    else
+                    {
+                        printf("\nWord has NOT been used this game. Added to used words.");
+                        for (int i = 0; i<=noUsedWords;i++)
+                        {
+                            printf("\nUsed word %d of %d is %s",i,noUsedWords,usedWords[i]);
+                        }
+                    }
+                    //check if word is already in the input file https://www.efaculty.in/c-programs/check-whether-a-given-word-exists-in-a-file-or-not-program-in-c/
                     FILE* filePointer;
                     int wordExist=0;
                     int bufferLength = 255;
                     char line[bufferLength];
-                    printf("\nFilename is %s",fname);
                     filePointer = fopen(fname, "r");
                     while(fgets(line, bufferLength, filePointer))
                     {
@@ -88,12 +158,12 @@ void main()
                     fclose(filePointer);
                     if (wordExist==1)
                     {
-                        printf("\nWord was already in file and NOT been used.");
-                        //add word to input.txt
+                        printf("\nWord was already in file.");
                     }
                     else 
                     {
-                        printf("\nWord doesn't exist in the file.");
+                        printf("\nWord doesn't exist in the file and will be added.");
+                        //add word to input file
                     }
                     exit(0);
                 }   
@@ -102,18 +172,21 @@ void main()
                     if (j<n)
                         continue;
                     printf("\n Invalid but part of it was at some point");
+                    //penalise
                     exit(0);
                 }
             }
             if (i==(n-1))
             {
                 printf("\n Word is not valid.");
+                //penalise
                 exit(0);
             }
         }
         else
         {
             printf("Word contains disallowed characters.");
+            //penalise
             exit(0);
         }
     }
