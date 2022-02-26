ac193bb8a0b8b8e25aa4f6d0b38217d5e82f460f
adamloec <adamloeckle@gmail.com>
2022-02-26 17:44:53 -0600

    Fixed it I guess

--- client/client.c
+++ client/client.c
@@ -154,7 +154,7 @@ int main()
                 strcpy(test, buffer);
                 printf("Turn: %s\n", test);
                 
-                /* if (strcmp(buffer, "turn") == 0)
+                if (strcmp(buffer, "turn") == 0)
                 {
                     // Players turn, recieves letters, list of words, etc.
                     char *letters;
@@ -176,7 +176,7 @@ int main()
                 else
                 {
                     // Idk
-                } */
+                }
             }
 
         }
@@ -203,51 +203,5 @@ int main()
         recv(clientSocket, buffer, 1024, 0);
         printf("Server: \t%s\n", buffer);
 	}
-	return 0;
-}
-
-int testClient(){
-
-	int clientSocket, ret;
-	struct sockaddr_in serverAddr;
-	char buffer[1024];
-
-	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-	if(clientSocket < 0){
-		printf("[-]Error in connection.\n");
-		exit(1);
-	}
-	printf("[+]Client Socket is created.\n");
-
-	memset(&serverAddr, '\0', sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = htons(PORT);
-	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-
-	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
-	if(ret < 0){
-		printf("[-]Error in connection.\n");
-		exit(1);
-	}
-	printf("[+]Connected to Server.\n");
-
-	while(1){
-		printf("Client: \t");
-		scanf("%s", &buffer[0]);
-		send(clientSocket, buffer, strlen(buffer), 0);
-
-		if(strcmp(buffer, ":exit") == 0){
-			close(clientSocket);
-			printf("[-]Disconnected from server.\n");
-			exit(1);
-		}
-
-		if(recv(clientSocket, buffer, 1024, 0) < 0){
-			printf("[-]Error in receiving data.\n");
-		}else{
-			printf("Server: \t%s\n", buffer);
-		}
-	}
-
 	return 0;
 }
