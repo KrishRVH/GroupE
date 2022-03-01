d2d559f32e9046fa4632ce16e838f1744e4447e9
therick5 <patricklav@protonmail.com>
2022-03-01 17:03:56 -0600

    fix

--- client/client.c
+++ client/client.c
@@ -238,7 +238,22 @@ int main()
                             while (1) {
                                 if (DEBUGGER) 
                                     printf("NEW ITERATION..\n");
-                                
+                                bzero(buffer, sizeof(buffer));
+                                recv(clientSocket, buffer, 1024, 0);
+                                sscanf(buffer, "%d", &noUsedWords);
+                                printf("Number of used words: %i\n", noUsedWords);
+                            
+                                if (noUsedWords != 0) {
+                                    if (DEBUGGER) 
+                                        printf("Waiting for # of used words..\n");
+                                    for (int i = 0; i < noUsedWords; i++)
+                                    {
+                                        bzero(buffer, sizeof(buffer));
+                                        recv(clientSocket, buffer, 1024, 0);
+                                        strcpy(usedWords[i], buffer);
+                                        printf("%s ", usedWords[i]);
+                                    }
+                                }
                                 // Player can submit a guess in under 4 minutes
                                 // Start time for 4 minutes
                                 //signal(SIGALRM, mySignal);
