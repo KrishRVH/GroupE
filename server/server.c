cc5a58cc63e41659ae7cff82fbbb04be5957a95e
adamloec <adamloeckle@gmail.com>
2022-03-02 17:12:14 -0600

    Test

--- server/server.c
+++ server/server.c
@@ -361,12 +361,12 @@ void playerTurn(int newSocket, struct Player *player, struct Computer *computer,
         strcpy(buffer, &num_resets);
         send(newSocket, buffer, 1024, 0);
         printf("NUM USED WORDS: %d", noUsedWords);
-        
+
         if (DEBUGGER) 
             printf("Sending # of used words..\n");
             // Sends number of used words and used words to client
             char num_used_words = noUsedWords + '0';
-            printf("NUM USED WORDS: %s", num_used_words);
+            printf("NUM USED WORDS: %c", num_used_words);
             bzero(buffer, sizeof(buffer));
             strcpy(buffer, &num_used_words);
             send(newSocket, buffer, 1024, 0);
